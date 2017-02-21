d51c528b235296c1a8116e3153485a3b945a8a11
dmatiasr <matu_dmr@hotmail.com>
2017-02-20 21:55:21 -0300

    Json utility convert

---
+++ src/main/java/Controller/JsonUtil.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import spark.*;
+
+public class JsonUtil {
+
+	public static String toJson(Object object) {
+		return new Gson().toJson(object);
+	}
+	public static ResponseTransformer json() {
+	    return JsonUtil::toJson;
+	  }
+}
