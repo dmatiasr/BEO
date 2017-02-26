e46339c75541cd8c36ecf1b809fc559b8d2b1cb3
dmatiasr <matu_dmr@hotmail.com>
2017-02-25 21:22:20 -0300

    check real status response

--- src/main/webapp/javascript/controllers/userController.js
+++ src/main/webapp/javascript/controllers/userController.js
@@ -10,7 +10,7 @@ angular.module('app')
       }
   ).success(function (data,status) {
     alert(status)
-    if(status==200){
+    if(status==201){
       $location.path("/");
       $window.location.reload();
     }
